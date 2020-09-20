5253d88a5bd784f68b1a0cb7e93a40f27e7647fa
Boidushya <boidushyabhattacharya@gmail.com>
2020-09-20 18:58:39 +0530

    fixed the dumb reddit marksdown support

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ class Bot:
 
 	　　ﾟ　　　.　　　. ,　　　　.　 .
 	"""]
-		botTag = "\n---\n<sub><sup>Beep boop I'm a bot. Also I'm the imposter ok bye. Made by u/boidushya</sup></sub>"
+		botTag = "\n---\n###### Beep boop I'm a bot. Also I'm the imposter ok bye. Made by u/boidushya"
 		return random.choice(choice)+botTag
 
 if __name__ == "__main__":
